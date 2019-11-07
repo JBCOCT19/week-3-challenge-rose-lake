5e5ebd9891db43c4badbd8b0c23edaeea69d26c2
rose-lake <klake12@montgomerycollege.edu>
2019-11-07 09:18:45 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+/**
+ * Thursday Weekly Challenge
+ * Week 3
+ * Resume Challenge
+ *
+ * Ksenia Lake
+ * November 7, 2019
+ *
+ * Project Description:
+ * Part 1: Robo Resume: https://canvas.instructure.com/courses/1743334/assignments/12845640?module_item_id=26321393
+ *
+ *
+ */
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+
+    }
+}
